8a524f8c35acc8d28e4ecf3ca26ca4a5db26d247
Mrkuzumi <mr.kuzumi0601@outlook.com>
2025-12-30 19:09:58 +0800

    学了如何用指针交换变量

---
+++ Estep/swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void swap(int *a,int *b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+   
+}
+int main(){
+    int a = 10;
+    int b = 20;
+    swap(&a,&b);
+    printf("%d %d",a,b);
+    return 0;
+
+}
